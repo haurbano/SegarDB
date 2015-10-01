d03fe1b7dcb4ab979669b031b36001d1bccb6ec9
haurbano <h.a.u.r1993@gmail.com>
2015-10-01 11:29:27 -0500

    Lista y floatingActionButton para agregar

--- app/src/main/java/com/moviles/salt/dbfacil/AlertAgregar.java
+++ app/src/main/java/com/moviles/salt/dbfacil/AlertAgregar.java
@@ -7,6 +7,8 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.EditText;
 
 import com.moviles.salt.dbfacil.entidades.Carro;
 
@@ -25,17 +27,35 @@ public class AlertAgregar extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
+        final EditText referencia, marca, modelo;
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         builder.setTitle("Agregar Carro");
+        View v;
+
+        v = View.inflate(getActivity(),R.layout.template_alert,null);
+
+        referencia = (EditText) v.findViewById(R.id.edit_referencia);
+        marca = (EditText) v.findViewById(R.id.edit_marca);
+        modelo = (EditText) v.findViewById(R.id.edit_modelo);
+
+        builder.setView(v);
 
         builder.setPositiveButton("Agregar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Carro car = new Carro();
 
+                car.setColor("rojo");
+                car.setImagen("url");
+                car.setMarca(marca.getText().toString());
+                car.setReferencia(referencia.getText().toString());
+                car.setModelo(2009);
 
                 car.save();
+
+                mainActivity.CargarCarros();
             }
         });
 
